35b961203634fe6178502ec54a67562e98b2e632
samet <kysamet@gmail.com>
2022-05-12 15:05:09 +0300

    Initial commit

---
+++ src/main/java/com/desi/desiframework/DesiFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.desi.desiframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesiFrameworkApplication.class, args);
+    }
+
+}
